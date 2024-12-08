ac2bbea5c7ca9e50f8d39a9c20b038bc1ebedd71
fameen <fameen@ncsu.edu>
2024-12-08 14:09:41 -0500

    Unsupervised embedding model getting ready to deploy...

--- src/my_kge.py
+++ src/my_kge.py
@@ -4,17 +4,25 @@ Module to make KG node embeddings by averaging neighboring node embeddings
 
 import argparse
 import re
+import os
+import csv
 import rdflib
 import numpy as np
+import torch
 import matplotlib.pyplot as plt
 from sentence_transformers import SentenceTransformer
 import networkx as nx
+# from torch.nn.functional import normalize
 from hdt import HDTDocument
+
+
+# DATASET PATH
+FILE_DIR = "./files/"
 
 
 class KGE:
     """
-    class KGE
+    class KGE. Simple knowledge graph embedding computation.
     """
 
     # Sentence transformer model name
@@ -27,7 +35,8 @@ class KGE:
 
     def __init__(self, files) -> None:
         """
-        @param files
+        Initialization.
+        @param files to be parsed.
         """
         self.graph = self.load_kg(files)
         self.model = SentenceTransformer(self.model_name)
@@ -35,7 +44,8 @@ class KGE:
 
     def load_kg(self, files) -> rdflib.Graph:
         """
-        @param files
+        Parses the given files to load the graphs.
+        @param files List of graph containing files that need to be loaded.
         """
         graph = rdflib.Graph()
         for f in files:
@@ -58,8 +68,9 @@ class KGE:
 
     def load_hdt(self, file, graph):
         """
-        @param file
-        :param graph
+        Loads graphs from hdt files.
+        @param graph the graph object to be initialized
+        @param file hdt file to be loaded
         """
         def node_type(node: str):
             if self.uri_regex.match(node):
@@ -69,27 +80,65 @@ class KGE:
             return rdflib.BNode()
 
         document = HDTDocument(file)
-        triples, cardinality = document.search_triples("", "", "")
+        triples, _ = document.search_triples("", "", "")
         for s, p, o in triples:
-            s = node_type(s)
             graph.add((node_type(s), node_type(p), node_type(o)))
 
     def load_neighbors(self, iri: str, print_len: bool = False) -> set:
         """
         Loading all node neighbors
-        :param print_len print the number of neighbors?
+        @param iri the iri of the node whose neighbors we want
+        @param print_len print the number of neighbors?
         """
         neighbors = set()
-        for o in self.graph.objects(subject=rdflib.URIRef(iri)):
-            # if isinstance(o, rdflib.Literal):
-            neighbors.add(o)
+        for node in self.graph.objects(subject=rdflib.URIRef(iri), unique=False):
+            neighbors.add(node)
         if print_len:
             print(f"{iri} has {len(list(neighbors))} neighbors")
         return neighbors
+    
+    def create_attr_val_embeddings(self, iri: str, minRows: int = 15) -> torch.Tensor:
+        """
+        Create tensor-based embeddings for node neighbors.
+        :param iri: The IRI of the node.
+        :return: A tensor of neighbor embeddings.
+        """
+        neighbors = []
+        
+        # Iterate through the predicate-object pairs of the given IRI
+        for p, o in self.graph.predicate_objects(subject=rdflib.URIRef(iri)):
+
+            # Extract the meaningful part of the predicate using regex
+            p = re.search(r'([^/#]+)$', str(p))
+
+            # Skip if Orcid Id
+            if p in ("orcidID", "P496"):
+                continue
+            
+            # Concatenate the predicate and object embeddings
+            encoded = torch.cat((torch.tensor(self.model.
+                                              encode(p, normalize_embeddings=True), dtype=torch.float32),
+                                torch.tensor(self.model.
+                                              encode(o.value, normalize_embeddings=True), dtype=torch.float32)))
+            neighbors.append(encoded)
+
+        # Stack all neighbor embeddings into a single tensor
+        if neighbors:
+            neighbors = torch.stack(neighbors)
+            rows, cols = neighbors.shape
+            if rows < minRows:
+                padding = torch.zeros((minRows - rows, cols))
+                neighbors = torch.cat((neighbors, padding), dim=0)
+            return neighbors
+
+        # Return an empty tensor if there are no neighbors
+        return torch.empty((0,), dtype=torch.float32)
 
     def create_neighbor_embeddings(self, iri: str, level2: bool = False) -> list:
         """
         Return embeddings of node neighbors.
+        @param iri the iri of the node whose neighbor's embeddings we want
+        @param level2 do we want the neighbors of its neighbors?
         """
         neighbors = self.load_neighbors(iri)
         neighbor_embeddings = []
@@ -97,7 +146,7 @@ class KGE:
 
         for node in neighbors:
             if isinstance(node, rdflib.Literal):
-                neighbor_embeddings.append(self.model.encode(node.value))
+                neighbor_embeddings.append(self.model.encode(node.value, normalize_embeddings=True))
 
             elif level2 and isinstance(node, rdflib.URIRef):
                 neighbor_iris.append(node.value)
@@ -106,7 +155,9 @@ class KGE:
 
     def create_embedding(self, iri: str, level2: bool = False) -> list:
         """
-        Embed one node.
+        Embed one node in the parsed graph.
+        @param iri the iri of the node to be embedded
+        @param2 do we want to consider the neighbors of its neighbors?
         """
         embedding = 0
         neighbor_embeddings, neighbor_iris = self.create_neighbor_embeddings(iri, level2)
@@ -130,21 +181,29 @@ class KGE:
 
         return embedding
 
-    def create_embeddings(self):
+    def create_embeddings(self, outfile: str = "embeddings.csv"):
         """
-        Embed all nodes.
+        Embed all nodes in the parsed graph.
+        @param outfile the file to write the node embeddings to
         """
         embeddings = {}
+        total = len(list(self.graph.subjects(unique=True)))
         # print(f"Graph contains {len(list(self.graph.subjects(unique=True)))} unique triples.")
         for n, s in enumerate(self.graph.subjects(unique=True)):
             val = str(s) if isinstance(s, rdflib.URIRef) else ""
             embedding = self.create_embedding(val)
             if not isinstance(embedding, int):
                 embeddings[val] = embedding
-                print(f"{s}: {n + 1} / {len(list(self.graph.subjects(unique=True)))}")
+                print(f"{s}: {n + 1} / {total}")
 
-        print(f"Embeddings contain {len(list(embeddings))} unique triple(s).")
+        # print(f"Embeddings contain {len(list(embeddings))} unique triple(s).")
+        with open(outfile, mode="w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(["IRI", "Embedding"])  # Write header
+            for val, embedding in embeddings.items():
+                writer.writerow([val, ",".join(map(str, embedding))])  # Convert embedding to CSV-friendly format
 
+        print(f"Embeddings saved to {outfile}.")
         return embeddings
 
     def node_match_all(self):
@@ -157,15 +216,16 @@ class KGE:
         labels = list(es.keys())
         embeddings = list(es.values())
 
-        for i in range(len(embeddings)):
-            for j in range(i + 1, len(embeddings)):
-                node_matches.append(
-                    (
-                        self.cosine_similarity(embeddings[i], embeddings[j]),
-                        labels[i],
-                        labels[j],
+        for i, e in enumerate(embeddings):
+            for j, e2 in enumerate(embeddings):
+                if i != j:
+                    node_matches.append(
+                        (
+                            np.dot(e, e2),
+                            labels[i],
+                            labels[j],
+                        )
                     )
-                )
 
         node_matches = sorted(node_matches, key=lambda x: x[0], reverse=True)
         print(f"Score count: {len(node_matches)}")
@@ -182,26 +242,27 @@ class KGE:
     def node_match_text(self, text):
         """
         match a node to text
+        @param text the text to match
         """
         node_matches = []
         close_matches = []
-        text_es = self.model.encode(text)
+        text_es = self.model.encode(text, normalize_embeddings=True)
         es = self.create_embeddings()
 
         labels = list(es.keys())
         embeddings = list(es.values())
 
-        for i in range(len(embeddings)):
+        for e, i in enumerate(embeddings):
             node_matches.append(
-                (self.cosine_similarity(embeddings[i], text_es), labels[i], text)
+                (labels[i], np.dot(e, text_es))
             )
 
-        node_matches = sorted(node_matches, key=lambda x: x[0], reverse=True)
+        node_matches = sorted(node_matches, key=lambda x: x[1], reverse=True)
         # print(f"Embeddings count: {len(es)} Score count: {len(matches)}")
-        print("Similarity Score: Node 1,      Node 2")
+        print("Node Name,      Similarity Score")
         for match in node_matches:
-            if match[0] > 0.4:
-                print(f"{match[0]}: {match[1].split("/")[-1]}, {match[2]}")
+            if match[1] > 0.4:
+                print(f"{match[0].split("/")[-1]}: {match[0]}")
                 close_matches.append(match)
             else:
                 break
@@ -211,7 +272,7 @@ class KGE:
     @staticmethod
     def cosine_similarity(es1, es2):
         """
-        :param es1, es2
+        @param es1, es2
         """
         similarity = np.dot(es1, es2) / (np.linalg.norm(es1) * np.linalg.norm(es2))
         return similarity
@@ -243,40 +304,41 @@ class KGE:
         plt.show()
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--files", type=str, nargs="+", help="List of ttl files")
-args = parser.parse_args()
-
 if __name__ == "__main__":
 
-    fileDir = "./files/"
+    # parse user arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file_dir",
+                        default=FILE_DIR,
+                        help="List of files to be parsed: ")
+    parser.add_argument("-t", "--text_match",
+                        default=None,
+                        help="Text to be matched against graph nodes")
+    parser.add_argument("-n", "--node_match",
+                        default=None,
+                        help="Match all graphs nodes against each other")
+    args = parser.parse_args()
 
-    if not args.files:
-        args.files = [fileDir + "climatepub4-kg.hdt"]
-        # args.files = [f+"jim.ttl", f+"james.ttl", f+"rada.ttl"]
-        # args.files = [f+"output.ttl"]
+    try:
+        # List all files in the directory
+        FILES = [
+            f for f in os.listdir(args.file_dir)
+            if os.path.isfile(os.path.join(args.file_dir, f))
+        ]
+        if len(FILES) == 0:
+            raise SystemExit("Error: No files includes in the files/ directory.")
+    except Exception as e:
+        raise SystemExit("Error parsing graph files in the files/ directory.") from e
 
-    myKGE = KGE(args.files)
+    myKGE = KGE(FILES)
     myKGE.create_embeddings()
+    
+    if args.text_match:
+        matches = myKGE.node_match_text(args.text_match)
+        if len(matches) > 0:
+            myKGE.view_graph(matches[0][1])
 
-    # myKGE.node_match_all()
-
-    matches = myKGE.node_match_text("SPECTROHELIOGRAPHS")
-    if len(matches) > 0:
-        myKGE.view_graph(matches[0][1])
-
-    # es = myKGE.es
-    # pca = PCA(n_components=2)
-    # reduced_embedding_pca = pca.fit_transform(list(es.values()))
-
-    # plt.figure(figsize=(8, 6))
-    # plt.scatter(reduced_embedding_pca[:, 0], reduced_embedding_pca[:, 1], c='blue', alpha=0.5)
-
-    # for i, label in enumerate(es.keys()):
-    #   plt.text(reduced_embedding_pca[i, 0], reduced_embedding_pca[i, 1], label, fontsize=9, ha='right')
-
-    # plt.title("3D Visualization of 384-Dimensional Embedding (PCA)")
-    # plt.xlabel("Principal Component 1")
-    # plt.ylabel("Principal Component 2")
-    # plt.ylabel("Principal Component 3")
-    # plt.show()
+    if args.node_match:
+        matches = myKGE.node_match_all()
+        if len(matches) > 0:
+            myKGE.view_graph(matches[0][1])
